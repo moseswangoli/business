75d5c8ab569c187e92475433ed23452de44774a0
Your Name <mwangoli3@gmail.com>
2026-04-06 11:16:41 +0300

    Fix homepage route

--- business_system/urls.py
+++ business_system/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('your site is live and working')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
